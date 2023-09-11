eb4e284b09e951e0bab15a285a7a1f5547c5ecfb
mohi14 <mohiuddinkhan1409@gmail.com>
2023-09-11 17:19:43 +0600

    git remote add origin https://github.com/MamtazDev/trky.gits

--- src/pages/category/[cateId].js
+++ src/pages/category/[cateId].js
@@ -2,10 +2,12 @@ import Item from "@/components/Item";
 import React, { useEffect } from "react";
 
 const Category = ({ products }) => {
-
   return (
     <div className="container">
-      <h3 className="text-center mt-5"> {products?.length > products.category?.cate_name}</h3>
+      <h3 className="text-center mt-5">
+        {" "}
+        {products?.length > products?.category?.cate_name}
+      </h3>
       <Item items={products} />
     </div>
   );
@@ -15,7 +17,9 @@ export async function getServerSideProps(context) {
   const { params } = context;
   const { cateId } = params;
 
-  const response = await fetch(`${process.env.REACT_APP_SERVER_URI}/products/category/${cateId}`);
+  const response = await fetch(
+    `${process.env.REACT_APP_SERVER_URI}/products/category/${cateId}`
+  );
   const data = await response.json();
   return {
     props: {
